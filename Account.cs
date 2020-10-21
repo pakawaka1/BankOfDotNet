f6dec54f136b5d905c2d3cf15d686e339303aca5
pakawaka1 <davidcervantesdev@gmail.com>
2020-10-21 07:05:52 -0700

    abstract account class created

---
+++ Account.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankOfDotNet
+{
+    public abstract class Account
+        {
+            protected Guid accountId;
+            protected double balance;
+
+        protected Account(double initialBalance);
+        {
+            AccountId = Guid.newGuid();
+            Balance = initialBalance;
+        }
+
+        public Guid AccountId
+        {
+            get { return accountId; }
+            set { accountId = value; }
+        }
+
+        public double Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+
+        public virtual void Deposit(double amount)
+        {
+            balance += amount;
+        }
+
+        public virtual void Withdraw(double amount)
+        {
+            if (amount <= balance)
+            {
+                balance -= amount;
+            }
+        }
+
+        public abstract void Display();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankOfDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
